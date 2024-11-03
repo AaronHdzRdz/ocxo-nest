47f4b0c0150c7fee8f98896d82b2b8a60cc729ce
Sebastián Aarón Hernández Reséndiz <shernandez114@alumnos.uaq.mx>
2024-11-02 20:14:12 -0600

    feat: Products page finished

--- app/dashboard/_components/_sidebar/Sidebar.tsx
+++ app/dashboard/_components/_sidebar/Sidebar.tsx
@@ -2,13 +2,13 @@ import { LuStore, LuUser, LuUsers, LuWheat, LuTruck } from 'react-icons/lu';
 import NavItem from './NavIterm';
 
 export default function Sidebar() {
-    return (
-        <nav className="w-1/12 h-[90vh] flex flex-col items-center py-5 bg-[#FEDCE1] justify-center gap-10">
-            <NavItem icon={<LuStore className='text-4xl' />} path="/dashboard" />
-            <NavItem icon={<LuTruck className='text-4xl' />} path="/dashboard/providers" />
-            <NavItem icon={<LuWheat className='text-4xl' />} path="/dashboard/products" />
-            <NavItem icon={<LuUser className='text-4xl' />} path="/dashboard/managers" />
-            <NavItem icon={<LuUsers className='text-4xl' />} path="/dashboard/employees" />
-        </nav>
-    )
+  return (
+    <nav className="w-1/12 min-w-[8.3333%] h-[90vh] bg-orange-200 flex flex-col items-center py-20 justify-center gap-10">
+      <NavItem icon={<LuStore className="text-4xl"/>} path="/dashboard"/>
+      <NavItem icon={<LuTruck className="text-4xl"/>} path="/dashboard/providers"/>
+      <NavItem icon={<LuWheat className="text-4xl"/>} path="/dashboard/products" />
+      <NavItem icon={<LuUser className="text-4xl"/> } path="/dashboard/managers"/>
+      <NavItem icon={<LuUsers className="text-4xl"/>} path="/dashboard/employees"/>
+    </nav>
+  );
 }
